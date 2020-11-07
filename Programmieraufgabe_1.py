bc134c71d94e79dd5ebd2fcd78aaf502dd7a0ef1
Hanno Seibod <hannoseibod@outlook.com>
2020-11-07 11:09:17 +0100

    Auspacken des Jupyter-Notebooks

---
+++ Programmieraufgabe_1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Programmieraufgabe 1: Gram-Schmidt Orthonormalisierungsverfahren
+
+# In[1]:
+
+
+import numpy as np
+from scipy.linalg import norm, hilbert
+
+
+# ## a)
+
+# In[2]:
+
+
+def gram_schmidt(V):
+    Q = np.copy(V)
+    ## Ihr Code hier
+    
+    return Q
+
+
+# ## b)
+
+# In[3]:
+
+
+H = hilbert(30)
+
+for n in [5,10,15,20]:
+    ## Ihr Code hier
+
+
+# ## c)
+
+# In[4]:
+
+
+def mod_gram_schmidt(V):
+    Q = np.copy(V)
+    ## Ihr Code hier
+    
+    return Q
+
+
+# ## d)
+
+# In[5]:
+
+
+for n in [5,10,15,20]:
+    ## Ihr Code hier
+
+
+# In[ ]:
+
+
+
+
