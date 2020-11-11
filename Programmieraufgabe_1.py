cb944c4e0e9a94788d97c7477e9407e9139bd2f5
Hanno Seibod <hannoseibod@outlook.com>
2020-11-11 21:00:28 +0100

    Formatierung (Strg+K,D) Verbesserung der b)

--- Programmieraufgabe_1.py
+++ Programmieraufgabe_1.py
@@ -4,7 +4,6 @@
 # # Programmieraufgabe 1: Gram-Schmidt Orthonormalisierungsverfahren
 
 # In[1]:
-
 import math
 import numpy as np
 from scipy.linalg import norm, hilbert
@@ -14,120 +13,125 @@ from scipy.linalg import norm, hilbert
 
 # In[2]:
 def skalarprodukt(V,W):#Normales Skalarprodukt
-    n=len(V)
-    x=0
-    i=0
-    while i<n:
-        x=x+V[i]*W[i]
-        i=i+1
+    n = len(V)
+    x = 0
+    i = 0
+    while i < n:
+        x = x + V[i] * W[i]
+        i = i + 1
     return x
 
 def multskalar(a,W):#Multiplikation: Skalar, Vektor
-    n=len(W)
-    B=[]
-    i=0
-    while i<n:
-        B.append(a*W[i])
-        i=i+1
+    n = len(W)
+    B = []
+    i = 0
+    while i < n:
+        B.append(a * W[i])
+        i = i + 1
     return B
 
 def subtrahiere(V,W):#Vektorsubtraktion
-    n=len(V)
-    B=[]
-    i=0
-    while i<n:
-        B.append(V[i]-W[i])
-        i=i+1
+    n = len(V)
+    B = []
+    i = 0
+    while i < n:
+        B.append(V[i] - W[i])
+        i = i + 1
     return B
 
 def gram_schmidt(A):
-    n=len(A)
-    m=len(A[0])
-    Q=A #wird am ende ausgegeben
-    spalte=1# da Q=A kann in Spalte 1, bzw 2, angefangen werden
-    while spalte<m:# die restlichenw erden durchgegangen
-        altespalte=[]
-        z=0
-        V=[]
-        while z<n:
-                V.append(Q[z][spalte-1])#V entpricht v_i
+    n = len(A)
+    m = len(A[0])
+    Q = A #wird am ende ausgegeben
+    spalte = 1# da Q=A kann in Spalte 1, bzw 2, angefangen werden
+    while spalte < m:# die restlichenw erden durchgegangen
+        altespalte = []
+        z = 0
+        V = []
+        while z < n:
+                V.append(Q[z][spalte - 1])#V entpricht v_i
                 altespalte.append(A[z][spalte])#erstes Element aus Iteration
-                z=z+1
-        i=0
-        neuespalte=altespalte
-        while i<spalte:
-            a=skalarprodukt(V,altespalte)
-            b=skalarprodukt(V,V)
-            C=multskalar(a/b,V)
-            neuespalte=subtrahiere(neuespalte, C)#entspricht der Summe
-            i=i+1        
-        t=0
-        while t<n:
-            Q[t][spalte]=neuespalte[t]#Eintr?ge in Q werden ge?ndert
-            t=t+1
-        spalte=spalte+1
+                z = z + 1
+        i = 0
+        neuespalte = altespalte
+        while i < spalte:
+            a = skalarprodukt(V,altespalte)
+            b = skalarprodukt(V,V)
+            C = multskalar(a / b,V)
+            neuespalte = subtrahiere(neuespalte, C)#entspricht der Summe
+            i = i + 1        
+        t = 0
+        while t < n:
+            Q[t][spalte] = neuespalte[t]#Eintr?ge in Q werden ge?ndert
+            t = t + 1
+        spalte = spalte + 1
     return Q
     
 #Testmatrix:
-A=[[3,2],[1,2],[2,2]]
-B=gram_schmidt(A)
+A = [[3,2],[1,2],[2,2]]
+B = gram_schmidt(A)
 print(B)
 #Ergebnnis nach Wikipedia: [[3,-4/7],[1,8/7],[2,2/7]]
 
 
 
 # ## b)
-
 H = hilbert(30)
 
-for n in [5,10,15,20]: #wir bauen uns hier eine kleinere Hilbert-Matrix (also aus einer 30x30 Matrix wird eine 30x5, 30x10,,,)
-    m=len(H) #Anzahl der Zeilen
-    j=0
-    HB=[]
-    while j<m:
-        V=[]
-        i=0
-        while i<n:
-            V.append(H[j][i])
-            i+=1
-        HB.append(V)
-        j+=1
-    Q=gram_schmidt(HB)
-    print('Für n= ',n,' gibt das Gram-Schmidt-Verfahren die Matrix ',Q,' zurück')
+for n in [5,10,15,20]: #wir bauen uns hier eine kleinere Hilbert-Matrix (also aus einer 30x30 Matrix
+                       #wird eine 30x5, 30x10,,,)
 
-    I=np.identity(n) #die Einheitsmatrix mit Länge/Breite n wird gebaut
-    Q_T=np.transpose(Q) #wir transponieren die Matrix Q
+    bisHier = 30 - n
+    print('Für n = ',n,' gibt das Gram-Schmidt-Verfahren folgende Matrix zurueck: \n', H[0:bisHier,0:bisHier], "\n\n") #Das ist die Macht von Numpy :D Vorteil: Zeilenumbrüche werden richtig
+                                                                                                                       #gesetzt.
+
+    #m=len(H) # m: Anzahl der Zeilen, n: Anzahl der Spalten
+    #j=0
+    #HB=[]
+    #while j<m:
+    #    V=[]
+    #    i=0
+    #    while i<n:
+    #        V.append(H[j][i])
+    #        i+=1
+    #    HB.append(V)
+    #    j+=1
+    #Q=gram_schmidt(HB)
+    #print('Für n= ',n,' gibt das Gram-Schmidt-Verfahren folgende Matrix
+    #zurueck: \n', Q)
+
+    #I=np.identity(n) #die Einheitsmatrix mit Länge/Breite n wird gebaut
+    #Q_T=np.transpose(Q) #wir transponieren die Matrix Q
    
-    prüfen=(1/n)*norm(I-np.matmul(Q_T,Q)) #wir prüfen, ob die Spalten von Q ein Orthonormalsystem bilden (Formel vom Blatt)
-    print('Das Prüfen der Matrix ergibt, dass n^(-1)||I-Q^(T)Q|| = ',prüfen)
+    #prüfen=(1/n)*norm(I-np.matmul(Q_T,Q)) #wir prüfen, ob die Spalten von Q
+    #ein Orthonormalsystem bilden (Formel vom Blatt)
+    #print('Das Prüfen der Matrix ergibt, dass n^(-1)||I-Q^(T)Q|| = ',prüfen)
 
 
 # ## c)
 
 # In[4]:
-
-
 def mod_gram_schmidt(A):
-    Q=A #wie in der a
-    n=len(A)
-    i=1
+    Q = A #wie in der a
+    n = len(A)
+    i = 1
     while i < n: #wir gehen wieder alle spalten durch
-        U=[]
-        a=0
-        while a<n: #startvektor der iteration
+        U = []
+        a = 0
+        while a < n: #startvektor der iteration
             U.append(Q[a][i])
             a+=1
-        j=0
-        while j<i: #hier wird U geändert, wie in der Formel
-           z=skalarprodukt(U,Q[i-1])
-           U=subtrahiere(U,multskalar(z,Q[i-1]))
+        j = 0
+        while j < i: #hier wird U geändert, wie in der Formel
+           z = skalarprodukt(U,Q[i - 1])
+           U = subtrahiere(U,multskalar(z,Q[i - 1]))
            j+=1
-        NORM=math.sqrt(skalarprodukt(U,U))#letztendlich noch deren Norm
-        d=0
-        p=1/NORM
-        X=multskalar(p,U)#das ist Definition der neuen Spalte
-        while d<n:# Übertragen der Spalte auf die Ausgabematrix
-            Q[d][i]=X[d]
+        NORM = math.sqrt(skalarprodukt(U,U))#letztendlich noch deren Norm
+        d = 0
+        p = 1 / NORM
+        X = multskalar(p,U)#das ist Definition der neuen Spalte
+        while d < n:# Übertragen der Spalte auf die Ausgabematrix
+            Q[d][i] = X[d]
             d+=1
         i+=1
     return Q
@@ -139,18 +143,18 @@ def mod_gram_schmidt(A):
 H = hilbert(30)
 
 for n in [5,10,15,20]:
-    m=len(H)
-    j=0
-    HB=[]
-    while j<m:
-        V=[]
-        i=0
-        while i<n:
+    m = len(H)
+    j = 0
+    HB = []
+    while j < m:
+        V = []
+        i = 0
+        while i < n:
             V.append(H[j][i])
             i+=1
         HB.append(V)
         j+=1
-    Q=mod_gram_schmidt(HB)
+    Q = mod_gram_schmidt(HB)
     print('Für n= ',n,' gibt das modifizierte Gram-Schmidt-Verfahren die Matrix ',Q,' zurück')
 
 
